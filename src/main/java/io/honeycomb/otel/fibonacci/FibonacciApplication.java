3fe263c128a8656b00ad854f6591d0679ba910c0
Jessica Kerr <jessitron@gmail.com>
2021-10-23 16:13:41 -0500

    Still have propagation

--- src/main/java/io/honeycomb/otel/fibonacci/FibonacciApplication.java
+++ src/main/java/io/honeycomb/otel/fibonacci/FibonacciApplication.java
@@ -7,19 +7,19 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-// import io.opentelemetry.context.propagation.ContextPropagators;
-// import io.opentelemetry.context.propagation.TextMapPropagator;
-// import io.opentelemetry.instrumentation.spring.autoconfigure.OpenTelemetryAutoConfiguration;
-// import java.util.Collections;
-// import java.util.List;
-// import org.springframework.beans.factory.BeanFactory;
-// import org.springframework.beans.factory.ObjectProvider;
-// import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-// import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-// import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-// import org.springframework.boot.context.properties.EnableConfigurationProperties;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.instrumentation.spring.autoconfigure.OpenTelemetryAutoConfiguration;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class FibonacciApplication {
