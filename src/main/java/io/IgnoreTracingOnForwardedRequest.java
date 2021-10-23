e2a12c0cb5c95e100e66fe2102ffb62ed0605c19
Jessica Kerr <jessitron@gmail.com>
2021-10-23 21:56:28 +0000

    Import cleanup
    This would be trivial in IntelliJ. It is not in VSCode.

--- src/main/java/io/IgnoreTracingOnForwardedRequest.java
+++ src/main/java/io/IgnoreTracingOnForwardedRequest.java
@@ -1,26 +1,11 @@
 package io.honeycomb.otel.fibonacci;
 
-import io.opentelemetry.api.OpenTelemetry;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.context.propagation.TextMapPropagator;
-import io.opentelemetry.instrumentation.spring.autoconfigure.OpenTelemetryAutoConfiguration;
 import java.util.Collections;
 import java.util.Collection;
 import java.util.List;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectProvider;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import io.opentelemetry.context.propagation.TextMapGetter;
 import io.opentelemetry.context.Context;
 
