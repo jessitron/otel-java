db77b9d1ff1b740fcf14df62c081c603eac11cd4
Jessica Kerr <jessitron@gmail.com>
2021-11-15 05:19:44 +0000

    how did this ever work

--- src/main/java/io/honeycomb/otel/fibonacci/Tracing.java
+++ src/main/java/io/honeycomb/otel/fibonacci/Tracing.java
@@ -1,8 +1,31 @@
 package io.honeycomb.otel.fibonacci;
 
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.context.propagation.TextMapGetter;
+import io.opentelemetry.context.Context;
+
+import io.opentelemetry.context.propagation.TextMapSetter;
+
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+
 public class Tracing {
 
   public static void setUpTracing() {
-     System. out.println("SET UP TRACING, YO");
+     System.out.println("SET UP TRACING, YO");
+
+      SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+        .addSpanProcessor(BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().build()).build())
+        .build();
+
+    OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+        .setTracerProvider(sdkTracerProvider)
+        .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+        .buildAndRegisterGlobal();
   }
 }
